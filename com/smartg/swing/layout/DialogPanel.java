8fe16a86cf2c9e1d58784ae165a7ac9aa0696428
Andrey Kuznetsov <andronix@gmx.net>
2018-03-25 22:14:31 +0200

    --

--- com/smartg/swing/layout/DialogPanel.java
+++ com/smartg/swing/layout/DialogPanel.java
@@ -33,158 +33,160 @@ import javax.swing.border.EmptyBorder;
  */
 public class DialogPanel extends GridPanel {
 
-	private static final long serialVersionUID = -6770092063928679808L;
+    private static final long serialVersionUID = -6770092063928679808L;
 
-	private JDialog currentDialog;
-	private final JButton closeButton = new JButton("Close");
-	private final JButton okayButton = new JButton("Okay");
-	private final Box buttonBox = Box.createHorizontalBox();
-	private final Box userButtonBox = Box.createHorizontalBox();
-	private boolean canceled = true;
-	private boolean closeDialogOnOkayClick = true;
-	// After each user Button we add a small strut, strutMap is used to remove
-	// proper strut, if we remove Button
-	private final Map<AbstractButton, Component> strutMap = new HashMap<>();
-	private String title;
-	private Predicate<DialogPanel> closePredicate;
+    private JDialog currentDialog;
+    private final JButton closeButton = new JButton("Close");
+    private final JButton okayButton = new JButton("Okay");
+    private final Box buttonBox = Box.createHorizontalBox();
+    private final Box userButtonBox = Box.createHorizontalBox();
+    private boolean canceled = true;
+    private boolean closeDialogOnOkayClick = true;
+    // After each user Button we add a small strut, strutMap is used to remove
+    // proper strut, if we remove Button
+    private final Map<AbstractButton, Component> strutMap = new HashMap<>();
+    private String title;
+    private Predicate<DialogPanel> closePredicate;
 
-	public DialogPanel() {
-		this(10);
-	}
+    public DialogPanel() {
+        this(10);
+    }
 
-	public DialogPanel(int gridWidth) {
-		super(gridWidth);
-		buttonBox.add(userButtonBox);
-		buttonBox.add(Box.createHorizontalGlue());
-		buttonBox.add(closeButton);
-		buttonBox.add(Box.createHorizontalStrut(10));
-		buttonBox.add(okayButton);
-		buttonBox.add(Box.createHorizontalStrut(20));
+    public DialogPanel(int gridWidth) {
+        super(gridWidth);
+        buttonBox.add(userButtonBox);
+        buttonBox.add(Box.createHorizontalGlue());
+        buttonBox.add(closeButton);
+        buttonBox.add(Box.createHorizontalStrut(10));
+        buttonBox.add(okayButton);
+        buttonBox.add(Box.createHorizontalStrut(20));
 
-		buttonBox.setBorder(new EmptyBorder(0, 10, 20, 0));
+        buttonBox.setBorder(new EmptyBorder(0, 10, 20, 0));
 
-		closeButton.addActionListener(e -> {
-			if (currentDialog != null && (closePredicate == null || closePredicate.test(this))) {
-				currentDialog.setVisible(false);
-			}
-		});
-		okayButton.addActionListener(e -> {
-			canceled = false;
-			if (closeDialogOnOkayClick && currentDialog != null) {
-				currentDialog.setVisible(false);
-			}
-		});
-	}
+        closeButton.addActionListener(e -> {
+            if (currentDialog != null && (closePredicate == null || closePredicate.test(this))) {
+                currentDialog.setVisible(false);
+            }
+        });
+        okayButton.addActionListener(e -> {
+            canceled = false;
+            if (closeDialogOnOkayClick && currentDialog != null) {
+                currentDialog.setVisible(false);
+            }
+        });
+    }
 
-	public Predicate<DialogPanel> getClosePredicate() {
-		return closePredicate;
-	}
+    public Predicate<DialogPanel> getClosePredicate() {
+        return closePredicate;
+    }
 
-	public void setClosePredicate(Predicate<DialogPanel> closePredicate) {
-		this.closePredicate = closePredicate;
-	}
+    public void setClosePredicate(Predicate<DialogPanel> closePredicate) {
+        this.closePredicate = closePredicate;
+    }
 
-	public final void addUserButton(AbstractButton b) {
-		userButtonBox.add(b);
-		Component strut = Box.createHorizontalStrut(10);
-		userButtonBox.add(strut);
-		strutMap.put(b, strut);
-	}
+    public final void addUserButton(AbstractButton b) {
+        userButtonBox.add(b);
+        Component strut = Box.createHorizontalStrut(10);
+        userButtonBox.add(strut);
+        strutMap.put(b, strut);
+    }
 
-	protected Box getButtonBox() {
-		return buttonBox;
-	}
+    protected Box getButtonBox() {
+        return buttonBox;
+    }
 
-	public void removeUserButton(AbstractButton b) {
-		userButtonBox.remove(b);
-		Component strut = strutMap.remove(b);
-		if (strut != null) {
-			userButtonBox.remove(strut);
-		}
-	}
+    public void removeUserButton(AbstractButton b) {
+        userButtonBox.remove(b);
+        Component strut = strutMap.remove(b);
+        if (strut != null) {
+            userButtonBox.remove(strut);
+        }
+    }
 
-	public final JButton getCloseButton() {
-		return closeButton;
-	}
+    public final JButton getCloseButton() {
+        return closeButton;
+    }
 
-	public final JButton getOkayButton() {
-		return okayButton;
-	}
+    public final JButton getOkayButton() {
+        return okayButton;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public boolean isCanceled() {
-		return canceled;
-	}
+    public boolean isCanceled() {
+        return canceled;
+    }
 
-	public boolean isCloseDialogOnOkayClick() {
-		return closeDialogOnOkayClick;
-	}
+    public boolean isCloseDialogOnOkayClick() {
+        return closeDialogOnOkayClick;
+    }
 
-	public void setCloseDialogOnOkayClick(boolean closeDialogOnOkayClick) {
-		this.closeDialogOnOkayClick = closeDialogOnOkayClick;
-	}
+    public void setCloseDialogOnOkayClick(boolean closeDialogOnOkayClick) {
+        this.closeDialogOnOkayClick = closeDialogOnOkayClick;
+    }
 
-	public final void setTitle(String title) {
-		this.title = title;
-	}
+    public final void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void showPanelInDialog(Component c) {
-		showPanelInDialog(SwingUtilities.getWindowAncestor(c));
-	}
+    public void showPanelInDialog(Component c) {
+        showPanelInDialog(SwingUtilities.getWindowAncestor(c));
+    }
 
-	public void showPanelInDialog(Window owner) {
-		JDialog dialog = new JDialog(owner, Dialog.ModalityType.APPLICATION_MODAL);
-		if (title != null) {
-			dialog.setTitle(title);
-		}
-		final Container contentPane = dialog.getContentPane();
-		final LayoutNode.VerticalNode root = new LayoutNode.VerticalNode("root");
-		root.setHorizontalAlignment(NodeAlignment.STRETCHED);
-		root.setVerticalAlignment(NodeAlignment.CENTER);
-		contentPane.setLayout(new JNodeLayout(contentPane, root));
-		contentPane.add(this, new NodeConstraints("root"));
-		contentPane.add(buttonBox, new NodeConstraints("root"));
+    public void showPanelInDialog(Window owner) {
+        JDialog dialog = new JDialog(owner, Dialog.ModalityType.APPLICATION_MODAL);
+        if (title != null) {
+            dialog.setTitle(title);
+        }
+        final Container contentPane = dialog.getContentPane();
+        final LayoutNode.VerticalNode root = new LayoutNode.VerticalNode("root");
+        root.setHorizontalAlignment(NodeAlignment.STRETCHED);
+        root.setVerticalAlignment(NodeAlignment.CENTER);
+        contentPane.setLayout(new JNodeLayout(contentPane, root));
+        contentPane.add(this, new NodeConstraints("root"));
+        contentPane.add(buttonBox, new NodeConstraints("root"));
 
-		JRootPane rootPane = dialog.getRootPane();
-		rootPane.setDefaultButton(getOkayButton());
+        JRootPane rootPane = dialog.getRootPane();
+        if (getOkayButton().isDefaultCapable()) {
+            rootPane.setDefaultButton(getOkayButton());
+        }
+ 
+        InputMap inputMap = rootPane.getInputMap(JButton.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        ActionMap actionMap = rootPane.getActionMap();
 
-		InputMap inputMap = rootPane.getInputMap(JButton.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-		ActionMap actionMap = rootPane.getActionMap();
+        KeyStroke ks = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+        String name = "CloseOnEscape";
+        inputMap.put(ks, name);
+        actionMap.put(name, new AbstractAction() {
+            private static final long serialVersionUID = 1L;
 
-		KeyStroke ks = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
-		String name = "CloseOnEscape";
-		inputMap.put(ks, name);
-		actionMap.put(name, new AbstractAction() {
-			private static final long serialVersionUID = 1L;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                closeButton.doClick();
+            }
+        });
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				closeButton.doClick();
-			}
-		});
+        dialog.pack();
+        dialog.setLocationRelativeTo(owner);
+        this.currentDialog = dialog;
+        dialog.setVisible(true);
+    }
 
-		dialog.pack();
-		dialog.setLocationRelativeTo(owner);
-		this.currentDialog = dialog;
-		dialog.setVisible(true);
-	}
+    public void showInPanel(JPanel contentPane) {
+        final LayoutNode.VerticalNode root = new LayoutNode.VerticalNode("root");
+        root.setHorizontalAlignment(NodeAlignment.STRETCHED);
+        root.setVerticalAlignment(NodeAlignment.CENTER);
+        contentPane.setLayout(new JNodeLayout(contentPane, root));
+        contentPane.add(this, new NodeConstraints("root"));
+        contentPane.add(buttonBox, new NodeConstraints("root"));
+    }
 
-	public void showInPanel(JPanel contentPane) {
-		final LayoutNode.VerticalNode root = new LayoutNode.VerticalNode("root");
-		root.setHorizontalAlignment(NodeAlignment.STRETCHED);
-		root.setVerticalAlignment(NodeAlignment.CENTER);
-		contentPane.setLayout(new JNodeLayout(contentPane, root));
-		contentPane.add(this, new NodeConstraints("root"));
-		contentPane.add(buttonBox, new NodeConstraints("root"));
-	}
-
-	/**
-	 * @return the currentDialog
-	 */
-	public JDialog getCurrentDialog() {
-		return currentDialog;
-	}
+    /**
+     * @return the currentDialog
+     */
+    public JDialog getCurrentDialog() {
+        return currentDialog;
+    }
 }
